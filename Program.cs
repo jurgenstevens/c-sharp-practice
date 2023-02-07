330a1698bce12a77c87103666522d8bc3886f6c5
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-07 00:35:04 -0600

    Update to fCC starter code.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
 
