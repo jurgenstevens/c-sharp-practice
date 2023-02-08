d50e017c1a9a5136f275866b9dced4d3084926e3
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-08 14:54:26 -0600

    Working with strings and simple methods.

--- Program.cs
+++ Program.cs
@@ -12,18 +12,32 @@ namespace Program
 // Everything within static void Main is what is gets executed
         static void Main(string[] args)
         {
-            string characterName = "Tom";
-            int characterAge;
-            characterAge = 25;
-
-            Console.WriteLine("There once was a man named " + characterName + ".");
-            Console.WriteLine("He was " + characterAge + " years old.");
-
-            characterName = "Mike";
-            Console.WriteLine("He really liked the name " + characterName + ".");
-            Console.WriteLine("But didn't like being " + characterAge + ".");
-
-
+            //string characterName = "Tom";
+            //int characterAge;
+            //characterAge = 25;
+            //Console.WriteLine("There once was a man named " + characterName + ".");
+            //Console.WriteLine("He was " + characterAge + " years old.");
+            //characterName = "Mike";
+            //Console.WriteLine("He really liked the name " + characterName + ".");
+            //Console.WriteLine("But didn't like being " + characterAge + ".");
+            string phrase = "Hello world.";
+            // can only be one character
+            char grade = 'A';
+            // int == numbers, duh
+            int age = 31;
+            // decimals can be stored as a float, a double, or a decimal. Go with double
+            double gpa = 3.2;
+            bool hired = true;
+            Console.WriteLine(phrase + " I'm " + age); // Hello world. I'm 31
+            Console.WriteLine(phrase.Length); // 12
+            Console.WriteLine(phrase.ToUpper()); // HELLO WORLD.
+            Console.WriteLine(phrase.ToLower()); // hello world.
+            Console.WriteLine(phrase.Contains("ello")); // true
+            Console.WriteLine(phrase[4]); // o
+            Console.WriteLine(phrase.IndexOf("world")); // 6
+            Console.WriteLine(phrase.IndexOf("z")); // -1 - non-existent
+            Console.WriteLine(phrase.Substring(6)); // world.
+            Console.WriteLine(phrase.Substring(6, 4)); // worl
 
             Console.ReadLine();
         }
