9912eafc0b03622e0af38eda78f1431bc3c2b35f
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-08 13:58:41 -0600

    Include variables.

--- Program.cs
+++ Program.cs
@@ -9,12 +9,22 @@ namespace Program
 {
     class Program
     {
+// Everything within static void Main is what is gets executed
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            string characterName = "Tom";
+            int characterAge;
+            characterAge = 25;
+
+            Console.WriteLine("There once was a man named " + characterName + ".");
+            Console.WriteLine("He was " + characterAge + " years old.");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName + ".");
+            Console.WriteLine("But didn't like being " + characterAge + ".");
+
+
+
             Console.ReadLine();
         }
     }
